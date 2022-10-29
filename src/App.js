f0f6bff79370c8e74c4ce176c23cc07388f7c2a2
Grant Pauker <grantpauker@gmail.com>
2022-10-29 13:12:11 -0700

    Add initial interfacing with firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { db } from './firebase';
+import { ref, get } from "firebase/database";
+
+const dbRef = ref(db);
+
+let dbVal = null;
+let dbString =null;
+get(dbRef).then((snapshot) => {
+  if (snapshot.exists()) {
+     dbVal = snapshot.val();
+  dbString = JSON.stringify(dbVal,null,4)
+    console.log(dbString);
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <h1>BruinYelp 😋</h1>
+    <h2>Eggert 🥺</h2>
+    <h3>Here is some JSON data (😎):</h3>
+      <pre>{dbString}</pre>
+  </div>
   );
 }
 
