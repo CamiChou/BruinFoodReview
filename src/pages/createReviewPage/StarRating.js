3e534c2695214313fef32960094f6feb6d8af325
Cami-Chou <camihchou@gmail.com>
2022-11-13 15:54:59 -0800

    works with pull now

--- src/pages/createReviewPage/StarRating.js
+++ src/pages/createReviewPage/StarRating.js
@@ -1,25 +1,38 @@
 import styles from "./styles.css";
 import React, { useState } from "react";
 
+const UNFILLED = "/Star1.png";
+const FIlLED = "/Star2.png";
+
+
 const StarRating = () => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
-  
   return (
     <div className="star-rating">
       {[...Array(5)].map((star, index) => {
         index += 1;
         return (
+        
+
           <button
+            class="starComp"
             type="button"
             key={index}
             className={index <= (hover || rating) ? "on" : "off"}
             onClick={() => setRating(index)}
             onMouseEnter={() => setHover(index)}
             onMouseLeave={() => setHover(rating)}
-          >
-            <span className="star">&#9733;</span>
+          > 
+            <img 
+            src = {index <= (hover || rating) ? FIlLED : UNFILLED}
+            style={{ width: 45, height: 45 }} 
+             >
+              
+            </img>
           </button>
+
+          
         );
       })}
     </div>
