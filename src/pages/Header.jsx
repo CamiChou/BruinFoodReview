72120ec07c797eea8eb8ca460f764523c4fa4442
Grant Pauker <grantpauker@gmail.com>
2022-11-25 23:25:30 -0800

    Remove duplciate import

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -11,12 +11,6 @@ import {
 } from "firebase/auth";
 import { getValueArrayWithinLimits } from "@appbaseio/reactivesearch/lib/utils";
 
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import { getValueArrayWithinLimits } from "@appbaseio/reactivesearch/lib/utils";
 class Header extends React.Component {
   constructor(props) {
     super(props);
