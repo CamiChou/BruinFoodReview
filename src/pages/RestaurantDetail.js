a194dce7fc5d75e4fb2b48f5d4731cac3605b0c7
dagny parayao <dagny.parayao@gmail.com>
2022-11-28 03:07:12 -0800

    styling changes for restaurant detail page, added picture functionality to restaurant detail page, added pictures to github repository, added star functionality to reviews

--- src/pages/RestaurantDetail.js
+++ src/pages/RestaurantDetail.js
@@ -54,6 +54,12 @@ function LoadResDesc() {
   return <RestaurantDesc>{desc}</RestaurantDesc>;
 }
 
+function LoadResPic() {
+  const params = useParams();
+
+  return <RestaurantPhoto src={`/rest-photos/${params.name}.jpeg`} />;
+}
+
 async function GrabReviewsNew(restName) {
   const topUserPostsRef = query(ref(db, "reviews"));
 
@@ -67,6 +73,7 @@ async function GrabReviewsNew(restName) {
           const review = {
             content: info.content,
             name: info.user,
+            stars: info.stars
           };
           reviews.push(review);
         });
@@ -81,6 +88,71 @@ async function GrabReviewsNew(restName) {
 
   let result = await reviews;
   return result;
+}
+
+function RenderStars(numStars) {
+  switch(numStars) {
+    case 0:
+      return (
+        <div>
+          <Star src={"/Star1.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+        </div>
+      )
+    case 1:
+      return (
+        <div>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+        </div>
+      )
+    case 2:
+      return (
+        <div>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+        </div>
+      )
+    case 3:
+      return (
+        <div>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+        </div>
+      )
+    case 4:
+      return (
+        <div>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star1.png"}></Star>
+        </div>
+      )
+    case 5:
+      return (
+        <div>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star2.png"}></Star>
+          <Star src={"/Star2.png"}></Star>
+        </div>
+      )
+  }
 }
 
 function LoadReviews() {
@@ -111,7 +183,10 @@ function LoadReviews() {
         <HoldReviews>
           {revData.map((rev, id) => (
             <ReviewBase key={id}>
-              <UserName>{rev.name}</UserName>
+              <ReviewTitleContainer>
+                <UserName>{rev.name}</UserName>
+                <ReviewStars>{RenderStars(rev.stars)}</ReviewStars>
+              </ReviewTitleContainer>
               <ReviewContent>{rev.content}</ReviewContent>
             </ReviewBase>
           ))}
@@ -130,11 +205,11 @@ function HandleInfo(props) {
       <ResContainer>
         <LoadResTitle />
         <RestaurantStars>
-          <StarRating />
+          {RenderStars(5)}
         </RestaurantStars>
         <LoadResDesc />
       </ResContainer>
-      <RestaurantPhoto src="/BruinYelp.png" />
+      <LoadResPic />
     </InfoContainer>
   );
 }
@@ -149,7 +224,8 @@ function RestaurantDetail() {
         <ReviewTitleContainer>
           <ReviewsTopTitle>Reviews</ReviewsTopTitle>
           <CreateReview to={review_path}>
-            <h2>Create Review</h2>
+            <Plus src="/CreateReviewPlus.png"/>
+            <h2 style={{marginTop: "5%", marginBottom: "5%", gridColumn: "2"}}>Create Review</h2>
           </CreateReview>
         </ReviewTitleContainer>
         <LoadReviews />
@@ -164,9 +240,9 @@ const InfoContainer = styled.div`
   margin: auto;
   margin-top: 5%;
   padding: 2%;
-  overflow-y: auto;
   width: 100%;
   max-width: 80vw;
+  max-height: 55vh;
   background-color: #d9d9d9;
   border-radius: 25px;
 `;
@@ -179,7 +255,7 @@ const DetailContainer = styled.div`
 `;
 
 const ResContainer = styled.div`
-  max-height: 100vh;
+  max-height: inherit;
   max-width: 100vw;
   display: grid;
   grid-template-rows: auto-fit;
@@ -189,12 +265,21 @@ const ResContainer = styled.div`
 
 const RestaurantTitle = styled.h1`
   align-self: center;
-  font-size: 2rem;
+  font-size: 3rem;
   grid-row: 1;
+  margin: 0%;
 `;
 
 const RestaurantStars = styled.div`
+  margin-top: auto;
+  margin-bottom: auto;
   grid-row: 2;
+`;
+
+const Star = styled.img`
+  width: 45px;
+  height: 45px;
+  padding-right: 2%;
 `;
 
 const RestaurantDesc = styled.div`
@@ -203,10 +288,13 @@ const RestaurantDesc = styled.div`
 `;
 
 const RestaurantPhoto = styled.img`
-  width: 20vw;
+  width: 90%;
   grid-column: 2;
   justify-self: right;
-  padding: 5%;
+  max-height: inherit;
+  object-fit: contain;
+  margin-top: auto;
+  margin-bottom: auto;
 `;
 
 const ReviewContainer = styled.div`
@@ -214,7 +302,6 @@ const ReviewContainer = styled.div`
   width: 100%;
   max-width: 80vw;
   margin: auto;
-  margin-top: 1%;
 `;
 
 const ReviewTitleContainer = styled.div`
@@ -222,29 +309,37 @@ const ReviewTitleContainer = styled.div`
   width: 100%;
   max-width: 80vw;
   margin: auto;
-  margin-top: 1%;
   grid-row: 1;
+  grid-template-columns: 75%;
 `;
 
 const ReviewsTopTitle = styled.h1`
   padding-top: 2%;
-  font-size: 2rem;
+  font-size: 2.5rem;
   grid-column: 1;
+  margin-bottom: 1%;
+`;
+
+const Plus = styled.img`
+  width: 70%;
+  grid-column: 1;
+  margin: auto;
 `;
 
 const CreateReview = styled(Link)`
   background-color: #3284bf;
   color: #efeeee;
-  border-radius: 25px;
+  border-radius: 18px;
   height: min-content;
   grid-column: 2;
   margin-top: auto;
-  margin-bottom: auto;
+  margin-bottom: 1%;
   max-width: fit-content;
   justify-self: flex-end;
-  padding-right: 2%;
+  padding-right: 4%;
   padding-left: 2%;
   text-decoration: none;
+  display: grid;
 `;
 
 const HoldReviews = styled.div`
@@ -277,7 +372,10 @@ const UserName = styled.h3`
 `;
 
 const ReviewStars = styled.div`
-  grid-row: 2;
+  margin-top: auto;
+  margin-bottom: auto;
+  grid-row: 1;
+  grid-column: 2;
 `;
 
 const ReviewContent = styled.div`
