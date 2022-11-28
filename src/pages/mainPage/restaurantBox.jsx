00e021ad77b3eda5edc8375742e847d4b32f984f
Paige <larsonpel181@gmail.com>
2022-11-27 20:02:52 -0500

    new main page layout/array created, still trying to figure out rendering

--- src/pages/mainPage/restaurantBox.jsx
+++ src/pages/mainPage/restaurantBox.jsx
@@ -1,11 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState,useEffect } from 'react';
+import mainPage from "./mainPage.css";
+
+import {db} from "../../firebase.js";
+import { getDatabase, ref, get, child, onValue } from "firebase/database";
+import { render } from '@testing-library/react';
+
+const dbRef=ref(getDatabase(), `restaurants/`)
+
+function loadName() {
+  let info=[]
+
+  onValue(dbRef, (snapshot)=>{
+    snapshot.forEach((childSnapshot)=>{
+      const childData=childSnapshot.val();
+      const childKey=childSnapshot.key;
+      info.push({Name:childData.name, Type:childData.type, Loc:childData.location})
+    });
+  }, {
+    onlyOnce:true
+  })
+   console.log("LOADING")
+   return info;
+   
+}
+
+
+/*
+function LoadRestaurant(props){
+  const [resName,setResName]=useState([]);
+  
+  const [resCuisine,setResCuisine]=useState([]);
+  const [resType,setResType]=useState([]);
+  const [resLoc,setResLoc]=useState([])
+  
+
+  return (
+
+        <>
+        <LoadName />
+        </>
+  );
+
+}
+*/
 
 function RestaurantBox() {
-    return (
-      <div>
-        This is RestaurantBox component
-      </div>        
-    );
+    
+    const info=loadName();
+    console.log("INFO VVVV")
+    console.log(info[0]);
+    return(
+      <div className="Box">
+        <div className="Tile">
+          
+        {info.map((resturant, id) => {console.log("HEY")})}
+        </div>
+      </div>
+    )
+
+
   }
    
   export default RestaurantBox;
