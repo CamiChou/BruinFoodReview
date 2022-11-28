6649e129e1e2fc82fab0e9add3dbc5571a1dc9d7
Grant Pauker <grantpauker@gmail.com>
2022-11-27 18:02:16 -0800

    Add asynchronous loading of restaurant boxes

--- src/pages/mainPage/restaurantBox.jsx
+++ src/pages/mainPage/restaurantBox.jsx
@@ -1,64 +1,60 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import mainPage from "./mainPage.css";
 
-import {db} from "../../firebase.js";
-import { getDatabase, ref, get, child, onValue } from "firebase/database";
-import { render } from '@testing-library/react';
+import { db } from "../../firebase.js";
+import { ref, get, child } from "firebase/database";
 
-const dbRef=ref(getDatabase(), `restaurants/`)
+const dbRef = ref(db);
 
-function loadName() {
-  let info=[]
-
-  onValue(dbRef, (snapshot)=>{
-    snapshot.forEach((childSnapshot)=>{
-      const childData=childSnapshot.val();
-      const childKey=childSnapshot.key;
-      info.push({Name:childData.name, Type:childData.type, Loc:childData.location})
+async function loadRestaurants() {
+  let restaurants = await get(child(dbRef, `restaurants`))
+    .then((snapshot) => {
+      let restaurant_buf = [];
+      if (snapshot.exists()) {
+        snapshot.forEach((childSnapshot) => {
+          const childData = childSnapshot.val();
+          restaurant_buf.push({
+            name: childData.name,
+            type: childData.type,
+            loc: childData.location,
+          });
+        });
+      }
+      return restaurant_buf;
+    })
+    .catch((error) => {
+      console.error(error.message);
     });
-  }, {
-    onlyOnce:true
-  })
-   console.log("LOADING")
-   return info;
-   
+  return restaurants;
 }
-
-
-/*
-function LoadRestaurant(props){
-  const [resName,setResName]=useState([]);
-  
-  const [resCuisine,setResCuisine]=useState([]);
-  const [resType,setResType]=useState([]);
-  const [resLoc,setResLoc]=useState([])
-  
-
-  return (
-
-        <>
-        <LoadName />
-        </>
-  );
-
-}
-*/
 
 function RestaurantBox() {
-    
-    const info=loadName();
-    console.log("INFO VVVV")
-    console.log(info[0]);
-    return(
-      <div className="Box">
-        <div className="Tile">
-          
-        {info.map((resturant, id) => {console.log("HEY")})}
-        </div>
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await loadRestaurants();
+      setRestaurants(data);
+      setLoading(false);
+    }
+    fetchData();
+  }, [restaurants]);
+  console.log(restaurants);
+  let restaurant_tiles;
+  if (isLoading) {
+    restaurant_tiles = <p>Restaurants are loading!</p>;
+  } else {
+    restaurant_tiles = restaurants.map((rest, id) => (
+      <div className="Tile">
+        Name: {rest.name}
+        Type: {rest.type}
+        Location: {rest.loc}
       </div>
-    )
-
-
+    ));
   }
-   
-  export default RestaurantBox;
+
+  return <div className="Box">{restaurant_tiles}</div>;
+}
+
+export default RestaurantBox;
