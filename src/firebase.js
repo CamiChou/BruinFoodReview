f86f2477905c5dd8b9dc4fbf3449d4d137bc48f2
Grant Pauker <grantpauker@gmail.com>
2022-11-28 22:24:56 -0800

    Migrate firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,21 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBFv_NIBqtpOz0TyjRYc4g2ou1ZNu6brKU",
-  authDomain: "cs35l-project-d0556.firebaseapp.com",
-  databaseURL: "https://cs35l-project-d0556-default-rtdb.firebaseio.com",
-  projectId: "cs35l-project-d0556",
-  storageBucket: "cs35l-project-d0556.appspot.com",
-  messagingSenderId: "691427087362",
-  appId: "1:691427087362:web:dbd69243c262b05396c31b",
-  measurementId: "G-DZRRBGRQW6",
+  apiKey: "AIzaSyAHtyLhPT31zK8wrIg1qp2I6LSjuplc0ps",
+  authDomain: "cs35l-project-ce551.firebaseapp.com",
+  databaseURL: "https://cs35l-project-ce551-default-rtdb.firebaseio.com",
+  projectId: "cs35l-project-ce551",
+  storageBucket: "cs35l-project-ce551.appspot.com",
+  messagingSenderId: "370167888366",
+  appId: "1:370167888366:web:01b141177e87658d8c36ff",
+  measurementId: "G-0YFW82R1CK",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
 export const auth = getAuth(app);
-
