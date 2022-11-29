b038e34d9a5325aaffab879ea58df5043836888b
Grant Pauker <grantpauker@gmail.com>
2022-11-29 15:07:55 -0800

    Migrate firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAHtyLhPT31zK8wrIg1qp2I6LSjuplc0ps",
-  authDomain: "cs35l-project-ce551.firebaseapp.com",
-  databaseURL: "https://cs35l-project-ce551-default-rtdb.firebaseio.com",
-  projectId: "cs35l-project-ce551",
-  storageBucket: "cs35l-project-ce551.appspot.com",
-  messagingSenderId: "370167888366",
-  appId: "1:370167888366:web:01b141177e87658d8c36ff",
-  measurementId: "G-0YFW82R1CK",
+  apiKey: "AIzaSyAKgXB06E25El6oKCV7qkkatKipjJkSXBI",
+  authDomain: "cs35l-project-c24eb.firebaseapp.com",
+  databaseURL: "https://cs35l-project-c24eb-default-rtdb.firebaseio.com",
+  projectId: "cs35l-project-c24eb",
+  storageBucket: "cs35l-project-c24eb.appspot.com",
+  messagingSenderId: "608807604396",
+  appId: "1:608807604396:web:5115453a9f7baa1c9f8f09",
+  measurementId: "G-E50TY6MSSV",
 };
 
 export const app = initializeApp(firebaseConfig);
