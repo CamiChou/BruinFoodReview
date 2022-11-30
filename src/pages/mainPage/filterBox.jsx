1791d5085b7bbac0c5b3241023735bcd393f813a
Alex <alexbranchpb@gmail.com>
2022-11-29 21:32:30 -0800

    Change filter button color when pressed

--- src/pages/mainPage/filterBox.jsx
+++ src/pages/mainPage/filterBox.jsx
@@ -4,32 +4,29 @@ import styled from "styled-components";
 import { db } from "../../firebase.js";
 import { ref, get, child } from "firebase/database";
 
-const theme = {
-  grayDefault: {
-    default: "#EFEEEE",
-    hover: "#90a4ae",
-  },
-  blueClick: {
-    default: "#3284bf",
-    hover: "#90a4ae",
-  },
-};
 
 const Button = styled.button`
-  background-color: ${(props) => theme[props.theme].default};
+  background-color: #EFEEEE;
   padding: 5px 10px;
   border-radius: 3px;
   outline: 0;
   transition: ease background-color 250ms;
   margin: 5px 5px;
   &:hover {
-    background-color: ${(props) => theme[props.theme].hover};
+    background-color: #90a4ae;
   }
 `;
 
-Button.defaultProps = {
-  theme: "grayDefault",
-};
+const ButtonToggle = styled(Button)`
+  background-color: #EFEEEE;
+  ${({ active }) =>
+    active &&
+    `
+    background-color: #3284bf;
+    &:hover {
+      background-color: #3284bf;
+  `}
+`;
 
 const dbRef = ref(db);
 
@@ -163,18 +160,22 @@ function FilterBox(props) {
   ];
 
   let button_grid = [];
+  // console.log(filter_buttons[0].buttons[0]['key']);
+  const [active, setActive] = useState(filter_buttons[0].buttons[0]['key']);
   filter_buttons.forEach((elem, i) => {
     let buttons = elem.buttons.map((button_elem, i) => {
       return (
-        <Button
+        <ButtonToggle
+          active = {active===button_elem.key}
           key={i}
           name={button_elem.key}
           onClick={(e) => {
+            setActive(button_elem.key);
             getFilteredResturants(e.target.name, props.setFilter);
           }}
         >
           {button_elem.name}
-        </Button>
+        </ButtonToggle>
       );
     });
     button_grid.push(
