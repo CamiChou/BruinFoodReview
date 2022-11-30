69e3dd323f1e67b5491f2ca941a4e4ccef0f64fa
Cami-Chou <camihchou@gmail.com>
2022-11-29 17:05:51 -0800

    some intermediary styling to main page

--- src/pages/mainPage/restaurantBox.jsx
+++ src/pages/mainPage/restaurantBox.jsx
@@ -65,9 +65,9 @@ const RestaurantBox = (props) => {
               className="restImages"
               src={rest.url}
               style={{
-                width: "15vw",
-                padding: "2%",
-                alignContent: "central",
+                width: "50%",
+                height: "100%",
+                borderRadius: "20px",
               }}
             />
             <h1 className="Text">{rest.name}</h1>
